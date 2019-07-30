5dd9013c855bf4dcff70c41fe6afc895ca624c2f
kinji <touyamakinji2532@gmail.com>
2019-07-30 16:43:46 +0900

    First commit

--- index.js
+++ index.js
@@ -262,8 +262,10 @@ client.on('message', message => {
     message.channel.send(uuid());
   }else if (message.content.match("chickenji")){
     message.delete(1);
-  }else if (message.content.match("いいぞ、もっといえ")){
+  }else if (message.content　==　"いいぞ、もっといえ"){
     message.channel.send("ばーかばーか");
+  }else if (message.content　==　"ばかはおまえだ( 'ω')"){
+    message.channel.send("うるせー");
   }else if (message.content === "れきゅ") {
   message.channel.send("呼んだ？");
   }else if (message.content.match("おはよ")) {
