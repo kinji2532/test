d2a99f63a5355c13c21d0c284edd3c766e1a9271
kinji <touyamakinji2532@gmail.com>
2019-08-24 15:19:13 +0900

    First commit

--- index.js
+++ index.js
@@ -609,7 +609,7 @@ uuid単体生成は/uだよ
   }
 
   }else if (message.content.startsWith("/")) {
-    message.channel.send(json1)
+    console.log(json1)
     // for(let co = 0;co < component.length;co ++){
     //   let com = message.content.replace("/","")
     //   if(com == ""){
