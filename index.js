555e5a61d179c649c3539e815bccf07605047896
kinji <touyamakinji2532@gmail.com>
2019-08-24 15:16:16 +0900

    First commit

--- index.js
+++ index.js
@@ -57,151 +57,154 @@ let chickenji = [
   "そういうこと言う人きらいです",
   "ばーかばーか"
 ]
-let component = [
-  {
-    "name":"minecraft:behavior.avoid_mob_type",
-    "value": `
-      "minecraft:behavior.avoid_mob_type": {
-        "entity_types": [
-          {
-            "filters": {
-              "test" :  "",
-              "subject" : "",
-               "value" :  ""
-            },
-            "max_dist": 6,
-            "walk_speed_multiplier": 1,
-            "sprint_speed_multiplier": 1.2
-          }
-        ]
-      },`
-},
-  {
-    "name":"minecraft:behavior.beg",
-    "value": `
-      "minecraft:behavior.beg": {
-        "look_distance": 1,
-        "look_time": 1,
-        "items": [ "" ]
-      },`
-  },
-  {
-    "name":"minecraft:behavior.break_doors",
-    "value": `
-      "minecraft:behavior.break_door": {}`
-  },
-  {
-    "name":"minecraft:behavior.stomp_turtle_egg",
-    "value": `
-    "minecraft:behavior.stomp_turtle_egg": {
-      "priority": 4,
-      "speed_multiplier": 1,
-      "search_range": 10,
-      "search_height": 3,
-      "goal_radius": 1.14,
-      "search_count": 4,
-      "interval": 20
-    },`
-  },
-  {
-    "name":"minecraft:behavior.breed",
-    "value": `
-    "minecraft:behavior.breed": {},`
-  },
-  {
-    "name":"minecraft:behavior.charge_held_item",
-    "value": `
-    "minecraft:behavior.charge_held_item": {
-      "items": [ "minecraft:arrow" ]
-    },`
-  },
-  {
-    "name":"minecraft：behavior.defend_village_target",
-    "value": `
-    "minecraft:behavior.defend_village_target": {
-      "filters": {},
-      "max_dist": 1,
-      "walk_speed_multiplier": 1,
-      "sprint_speed_multiplier": 1,
-      "must_see": 1,
-      "must_see_forget_duration": 1
-    },`
-  },
-  {
-    "name":"minecraft：behavior.drink_potion",
-    "value": `
-    "minecraft:behavior.drink_potion": {
-      "priority": 1,
-      "speed_modifier": 0.0,
-      "potions": [
-        {
-          "id": 7,
-          "chance": 1.0,
-          "filters": {}
-        }
-      ]
-    },`
-  },
-  {
-    "name":"minecraft：behavior.door_interact",
-    "value": `
-      "minecraft：behavior.door_interact": {},`
-  },
-  {
-    "name":"minecraft：behavior.eat_block",
-    "value": `
-    "minecraft:behavior.eat_block": {
-      "on_eat": {
-        "event": "",
-        "target": "self"
-      }
-    },`
-  },
-  {
-    "name":"minecraft：behavior.explore_outskirts",
-    "value": ``
-  },
-  {
-    "name":"minecraft：behavior.flee_sun",
-    "value": `
-    "minecraft:behavior.flee_sun": {
-      "speed_multiplier": 3
-    },`
-  },
-  {
-    "name":"minecraft：behavior.float",
-    "value": `
-      "minecraft：behavior.float": {},`
-  },
-  {
-    "name":"minecraft：behavior.follow_owner",
-    "value": `
-    "minecraft:behavior.follow_owner": {
-      "speed_multiplier": 1.0,
-      "start_distance": 10,
-      "stop_distance": 2
-    },`
-  },
-  {
-    "name":"minecraft：behavior.follow_parent",
-    "value": `
-    "minecraft:behavior.follow_parent": {
-      "speed_multiplier": 1.1
-    },`
-  },
-  {
-    "name":"",
-    "value": ``
-  },
-  {
-    "name":"",
-    "value": ``
-  },
-  {
-    "name":"",
-    "value": ``
-  }
-]
+// let component = [
+//   {
+//     "name":"minecraft:behavior.avoid_mob_type",
+//     "value": `
+//       "minecraft:behavior.avoid_mob_type": {
+//         "entity_types": [
+//           {
+//             "filters": {
+//               "test" :  "",
+//               "subject" : "",
+//                "value" :  ""
+//             },
+//             "max_dist": 6,
+//             "walk_speed_multiplier": 1,
+//             "sprint_speed_multiplier": 1.2
+//           }
+//         ]
+//       },`
+// },
+//   {
+//     "name":"minecraft:behavior.beg",
+//     "value": `
+//       "minecraft:behavior.beg": {
+//         "look_distance": 1,
+//         "look_time": 1,
+//         "items": [ "" ]
+//       },`
+//   },
+//   {
+//     "name":"minecraft:behavior.break_doors",
+//     "value": `
+//       "minecraft:behavior.break_door": {}`
+//   },
+//   {
+//     "name":"minecraft:behavior.stomp_turtle_egg",
+//     "value": `
+//     "minecraft:behavior.stomp_turtle_egg": {
+//       "priority": 4,
+//       "speed_multiplier": 1,
+//       "search_range": 10,
+//       "search_height": 3,
+//       "goal_radius": 1.14,
+//       "search_count": 4,
+//       "interval": 20
+//     },`
+//   },
+//   {
+//     "name":"minecraft:behavior.breed",
+//     "value": `
+//     "minecraft:behavior.breed": {},`
+//   },
+//   {
+//     "name":"minecraft:behavior.charge_held_item",
+//     "value": `
+//     "minecraft:behavior.charge_held_item": {
+//       "items": [ "minecraft:arrow" ]
+//     },`
+//   },
+//   {
+//     "name":"minecraft：behavior.defend_village_target",
+//     "value": `
+//     "minecraft:behavior.defend_village_target": {
+//       "filters": {},
+//       "max_dist": 1,
+//       "walk_speed_multiplier": 1,
+//       "sprint_speed_multiplier": 1,
+//       "must_see": 1,
+//       "must_see_forget_duration": 1
+//     },`
+//   },
+//   {
+//     "name":"minecraft：behavior.drink_potion",
+//     "value": `
+//     "minecraft:behavior.drink_potion": {
+//       "priority": 1,
+//       "speed_modifier": 0.0,
+//       "potions": [
+//         {
+//           "id": 7,
+//           "chance": 1.0,
+//           "filters": {}
+//         }
+//       ]
+//     },`
+//   },
+//   {
+//     "name":"minecraft：behavior.door_interact",
+//     "value": `
+//       "minecraft：behavior.door_interact": {},`
+//   },
+//   {
+//     "name":"minecraft：behavior.eat_block",
+//     "value": `
+//     "minecraft:behavior.eat_block": {
+//       "on_eat": {
+//         "event": "",
+//         "target": "self"
+//       }
+//     },`
+//   },
+//   {
+//     "name":"minecraft：behavior.explore_outskirts",
+//     "value": ``
+//   },
+//   {
+//     "name":"minecraft：behavior.flee_sun",
+//     "value": `
+//     "minecraft:behavior.flee_sun": {
+//       "speed_multiplier": 3
+//     },`
+//   },
+//   {
+//     "name":"minecraft：behavior.float",
+//     "value": `
+//       "minecraft：behavior.float": {},`
+//   },
+//   {
+//     "name":"minecraft：behavior.follow_owner",
+//     "value": `
+//     "minecraft:behavior.follow_owner": {
+//       "speed_multiplier": 1.0,
+//       "start_distance": 10,
+//       "stop_distance": 2
+//     },`
+//   },
+//   {
+//     "name":"minecraft：behavior.follow_parent",
+//     "value": `
+//     "minecraft:behavior.follow_parent": {
+//       "speed_multiplier": 1.1
+//     },`
+//   },
+//   {
+//     "name":"",
+//     "value": ``
+//   },
+//   {
+//     "name":"",
+//     "value": ``
+//   },
+//   {
+//     "name":"",
+//     "value": ``
+//   }
+// ]
+let pass = __dirname
+let json1 = require(pass + '/component.json')
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity('れきゅねこ教',{
@@ -606,14 +609,15 @@ uuid単体生成は/uだよ
   }
 
   }else if (message.content.startsWith("/")) {
-    for(let co = 0;co < component.length;co ++){
-      let com = message.content.replace("/","")
-      if(com == ""){
-        return;
-      }else if(component[co].name.match(com)){
-        message.channel.send("```\n" + component[co]["value"] + "```")
-      }
-    }
+    message.channel.send(json1)
+    // for(let co = 0;co < component.length;co ++){
+    //   let com = message.content.replace("/","")
+    //   if(com == ""){
+    //     return;
+    //   }else if(component[co].name.match(com)){
+    //     message.channel.send("```\n" + component[co]["value"] + "```")
+    //   }
+    // }
   }else{
   let random = Math.floor(Math.random() * 5);
   if (random == 0){
