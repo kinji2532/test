557053203434a0e02623ae845ce107fca6d53323
星伽　白雪 <hotogisirayuki9063@gmail.com>
2019-10-22 23:13:13 +0900

    First commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, Attachment } = require('discord.js');
 const client = new Client();
 const fs = require('fs');
 const cron = require('node-cron');
-let one,two,logmessage,el,dummy,replay,status,chickenji,memo,statusedit,memoedit;
+let one,two,logmessage,el,dummy,replay,status,chickenji,memo,statusedit,memoedit,component;
 let dl = [];
 let saymode = true
 /*let component = [
