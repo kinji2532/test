ccc6b4c46486de6796f1ab26550e29aae8045933
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2020-06-08 14:37:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,37 +25,7 @@ process.on('uncaughttException',(err)=>{
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity('れきゅねこ教',{
-    type : 'PLAYING'
-  })
-  client.channels.get('599272915153715201').bulkDelete(100)
-  client.channels.get('599272915153715201').send("リログしました。")
-  let url = 'https://www.dropbox.com/s/9hw6i2tdkz1dpf8/index-reki.js?dl=1'
-  let text = fs.createWriteStream('reki.js');
-  request.get(url).on('error',console.error).pipe(text)
-  text.on('finish',()=>{
-    try{
-      let txt = fs.readFileSync('reki.js','utf-8');
-      eval(txt);
-    }catch(e){
-      client.channels.get('599272915153715201').send(`エラーが起きたよ！\n${e.message}`);
-    }
-    fs.unlinkSync('reki.js')
-  })
+  client.channels.get('599272915153715201').send("test")
 });
 
-/*
-https://dashboard.heroku.com/apps/kinjisbot/resources
-cd discordbot2/
-git init
-heroku git:remote -a kinjisbot
-git add .
-git commit -m "First commit"
-git push heroku master --force
-*/
-
-/*memo
-let statusfile = new Attachment(pass + '/status.json')
-message.channel.send(statusfile);
-*/
 client.login(process.env.BOT_TOKEN);
