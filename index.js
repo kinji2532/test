0d779d5069625816cae62def07868f5b6ef76a4e
kinji2532 <46149200+kinji2532@users.noreply.github.com>
2020-07-05 20:43:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,4 @@ client.on('message', message=>{
   }
 });
 
-process.on('unhandledRejection',async error => {
-  client.channels.cache.get('599272915153715201').send('unhandled:'+inspect(error),{split:true}).then(msg=>msg.react('721260517875777546'))
-});
-
 client.login(process.env.BOT_TOKEN);
